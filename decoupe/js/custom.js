a2a335fa0e15dfb0279bedd9184b7b13ba22640c
julkwel <julienrajerison5@gmail.com>
2019-01-25 07:41:49 +0300

    [#Update] : env.dist configuration

--- decoupe/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.kl-scroll-icon').click(function(){
-		var theLink = $(this).attr('href');
-		$("html, body").animate({ scrollTop: $(theLink).offset().top }, 1000);
-		return false;
-	});
-});
